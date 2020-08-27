8d55b9cdc70e42953e3f602cf46067574a672135
Maximiliane Liffler <maxi-liffler@web.de>
2020-08-27 21:20:11 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,5 +1,6 @@
 function forLoop() {
 for (let i = 0; i < 25; i++){
-if (i === 0) {console.log("I am a strange loop.")}
-else {console.log("I am ${i} strange loops.")}
+if (i === 0) {console.log("I am a strange loop.")};
+else {console.log("I am ${i} strange loops.")};
+return forLoop();
 }
